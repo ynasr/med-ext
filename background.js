8732337e432605b93681fb84df4709f170597c49
Yazen Nasr <ynasr@Yazens-MBP.lan>
2022-12-08 21:09:48 -0800

    Remove background file and set limit to history added

--- background.js
+++
@@ -1,2 +0,0 @@
-self.importScripts("thirdParty/jquery-3.6.1.min.js");
-self.importScripts("thirdParty/autocomplete-lhc.min.js");
--- js/dyanmicForm.js
+++ js/dyanmicForm.js
@@ -1,9 +1,11 @@
 $(".add").click(function() {
     //$("form > p:last-child").clone(true).insertAfter("form > p:last-child");
     //return false;
-    $('span#add').before(`Pertinent History #${element.length}: <input type="text" id="condition${element.length}" name="condition"><br>`)
-    //$( "form" ).append( $( `<input type="text" id="condition${element.length}">` ) );
-    runTie();
+    if (element.length < 6) {
+        $('span#add').before(`Pertinent Medical History #${element.length}: <input type="text" id="condition${element.length}" name="condition"><br>`)
+        //$( "form" ).append( $( `<input type="text" id="condition${element.length}">` ) );
+        runTie();
+    }
 });
 
 $(".remove").click(function() {
